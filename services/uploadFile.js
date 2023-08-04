79d70dae743780228ea944d28b06b72e5ace765d
lucasdev2050 <141372093+lucasdev2050@users.noreply.github.com>
2023-08-04 18:35:26 -0300

    asdaw33

--- services/uploadFile.js
+++ services/uploadFile.js
@@ -1,10 +1,7 @@
 const fetch = require("node-fetch");
 const AWS = require("aws-sdk");
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
+const s3 = new AWS.S3();
 
 const filePathValues = "filteredTaxID.txt";
 const filePath = "./uploads/Padron-CABA.txt";
