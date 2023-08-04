ede2aacef4472ad94e22834bcbfaf1000c466547
lucasdev2050 <141372093+lucasdev2050@users.noreply.github.com>
2023-08-04 20:06:20 -0300

    asdlaksdmjnlre

--- services/uploadFile.js
+++ services/uploadFile.js
@@ -37,7 +37,7 @@ async function filterFileContent() {
 
   try {
     const dataArray = (await fs.readFile(filePathValues, "utf-8")).split("\n").map((line) => line.trim());
-    console.log(dataArray)
+    console.log("dataArray:" + dataArray)
     const fileContent = await fs.readFile(filePath, "utf-8");
     const lines = fileContent.split("\n");
 
@@ -46,7 +46,7 @@ async function filterFileContent() {
     const result = filteredLines.join("\n");
     await fs.writeFile(nuevoArchivo, result, "utf-8");
 
-    console.log(result);
+    console.log("result:" + result);
     
     console.log("El archivo ha sido filtrado exitosamente.");
   } catch (error) {
