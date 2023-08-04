f1db6d1a9057347e15cbc123c2877a0f21f3d1f3
lucasdev2050 <141372093+lucasdev2050@users.noreply.github.com>
2023-08-04 20:42:27 -0300

    dawkjsnaksjn

--- services/uploadFile.js
+++ services/uploadFile.js
@@ -1,17 +1,16 @@
 const fs = require("fs/promises");
 const fetch = require("node-fetch");
 const path = require("path");
-const shell = require('shelljs')
+const git = require("nodegit");
 
 const repoURL = "https://github.com/lucasdev2050/sapbyd-midd-nodejs";
-const repoPath = path.join("/tmp", ""); // Ruta temporal para clonar el repositorio
+const repoPath = path.join("/tmp", "temp-repo"); // Ruta temporal para clonar el repositorio
 const filePath = path.join(repoPath, "./uploads/Padron-CABA.txt");
 
 async function cloneRepoAndReadFile() {
   try {
     // Clonar el repositorio
-    shell.cd(repoPath)
-    shell.exec("git clone " + repoURL)
+    await git.Clone(repoURL, repoPath);
 
   } catch (error) {
     console.error("Error:", error);
