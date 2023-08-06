105e3463e20311482937ca4d888e14728611efef
lucasdev2050 <141372093+lucasdev2050@users.noreply.github.com>
2023-08-05 21:04:42 -0300

    sadkjadnakj334

--- services/uploadFile.js
+++ services/uploadFile.js
@@ -1,18 +1,17 @@
 const fs = require("fs/promises");
 const fetch = require("node-fetch");
 const path = require("path");
-const simpleGit = require("simple-git");
+const git = require("nodegit");
 
 const repoURL = "https://github.com/lucasdev2050/sapbyd-midd-nodejs";
 const repoPath = path.join("/tmp", "temp-repo"); // Ruta temporal para clonar el repositorio
-const filePath = path.join(repoPath, "./uploads/Padron-CABA.txt");
+const filePath = "./uploads/Padron-CABA_1.txt";
+const filePath2 = "./uploads/Padron-CABA_2.txt";
 
 async function cloneRepoAndReadFile() {
   try {
     // Clonar el repositorio
-    const git = simpleGit();
-
-    await git.clone(repoURL, repoPath);
+    await git.Clone(repoURL, repoPath);
 
   } catch (error) {
     console.error("Error:", error);
@@ -22,7 +21,6 @@ async function cloneRepoAndReadFile() {
 cloneRepoAndReadFile();
 
 const filePathValues = path.join("/tmp", "filteredTaxID.txt");
-// const filePath = "./uploads/Padron-CABA.txt";
 const nuevoArchivo = path.join("/tmp", "nuevoArchivo.txt");
 // const padronCabaJson = "./uploads/padron-caba.json";
 
@@ -56,11 +54,16 @@ async function filterFileContent() {
     const dataArray = (await fs.readFile(filePathValues, "utf-8")).split("\n").map((line) => line.trim());
     console.log("dataArray:" + dataArray)
     const fileContent = await fs.readFile(filePath, "utf-8");
+    const fileContent2 = await fs.readFile(filePath2, "utf-8");
+
     const lines = fileContent.split("\n");
+    const lines2 = fileContent2.split("\n");
+
+    const mergedArray = [...lines, ...lines2];
 
     console.log("fileContent:" + fileContent)
 
-    const filteredLines = lines.filter((line) => dataArray.some((value) => line.includes(value)));
+    const filteredLines = mergedArray.filter((line) => dataArray.some((value) => line.includes(value)));
 
     console.log("filteredLines:" + filteredLines)
 
@@ -76,3 +79,65 @@ async function filterFileContent() {
 }
 
 module.exports = { filterFileContent }
+
+
+
+// const fs = require("fs/promises");
+// const fetch = require("node-fetch");
+
+
+// // const fs = require("@cyclic.sh/s3fs/promises")(process.env.S3_BUCKET_NAME, config);
+
+// const filePathValues = "./uploads/filteredTaxID.txt";
+// const filePath = "./uploads/Padron-CABA_1.txt";
+// const filePath2 = "./uploads/Padron-CABA_2.txt";
+// // const padronCabaJson = "./uploads/padron-caba.json";
+
+// async function fetchDataFromByD() {
+//   const auth = "Basic " + Buffer.from(process.env.BYDUSERNAME + ":" + process.env.BYDPASSWORD).toString("base64");
+//   const headers = {
+//     Authorization: auth,
+//     "x-csrf-token": "fetch",
+//     "Accept-Encoding": "gzip, deflate, br",
+//   };
+
+//   const response = await fetch(process.env.ODATAURI, { headers });
+//   const data = await response.json();
+//   return data.d.results;
+// }
+
+// async function writeFilteredTaxIDs(data) {
+//   const filteredTaxIDs = data
+//     .filter((obj) => obj.CountryCode === "AR")
+//     .map((obj) => obj.PartyTaxID)
+//     .join("\n");
+  
+//   await fs.writeFile(filePathValues, filteredTaxIDs, "utf-8");
+// }
+
+// async function filterFileContent() {
+//   const dataJson = await fetchDataFromByD();
+//   await writeFilteredTaxIDs(dataJson);
+
+//   try {
+//     const dataArray = (await fs.readFile(filePathValues, "utf-8")).split("\n").map((line) => line.trim());
+//     const fileContent = await fs.readFile(filePath, "utf-8");
+//     const fileContent2 = await fs.readFile(filePath2, "utf-8");
+   
+//     const lines = fileContent.split("\n");
+//     const lines2 = fileContent2.split("\n");
+
+//     const mergedArray = [...lines, ...lines2];
+
+//     const filteredLines = mergedArray.filter((line) => dataArray.some((value) => line.includes(value)));
+
+//     const result = filteredLines.join("\n");
+//     await fs.writeFile("./uploads/nuevoArchivo.txt", result, "utf-8");
+    
+//     console.log("El archivo ha sido filtrado exitosamente.");
+//   } catch (error) {
+//     console.error("Error:", error.message);
+//   }
+// }
+
+// module.exports = { filterFileContent }
